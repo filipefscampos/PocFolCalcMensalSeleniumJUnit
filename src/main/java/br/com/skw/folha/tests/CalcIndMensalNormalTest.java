f97ebf6b0ccba4820d0f914c92f3abc00bc7fb75
Filipe Campos <felipetwo@gmail.com>
2022-07-03 21:56:06 -0300

    Metodo escreverDepoisTab e melhora do codEmp codFunc

--- src/main/java/br/com/skw/folha/tests/CalcIndMensalNormalTest.java
+++ src/main/java/br/com/skw/folha/tests/CalcIndMensalNormalTest.java
@@ -15,21 +15,24 @@ public class CalcIndMensalNormalTest extends BaseTest {
 	MenuPage menuPage = new MenuPage();
 	CalculoPage calcPage = new CalculoPage();
 	
-	@DisplayName("Testa o calculo individual mensal de um funcionário normal 30 dias")
+	@DisplayName("Calculo Individual Mensal 30 dias Normal")
 	@Test
-	public void calcMensalIndividual() {
+	public void calcMensalIndividual() throws InterruptedException {
 		getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
 		menuPage.abreTelaCalculoFolha();
-		
-		getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
+		//getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		Thread.sleep(2000);
 		calcPage.abreCalculoIndividual();
-		
+		getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		calcPage.abreCardCalculoIndividualMensal();
 		
 		calcPage.preencheReferencia("07/2022");
 		calcPage.preencheDataPagamento("29/07/2022");
+		
+		calcPage.preencheCodEmp("2");
+		//calcPage.preencheCodFunc("14");
+		Thread.sleep(5000);
+		
 		
 	}
 	
