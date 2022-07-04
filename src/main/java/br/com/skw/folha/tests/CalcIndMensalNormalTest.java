302672c49e75d3d3525a62630606cd9beaa480ab
Filipe Campos <felipetwo@gmail.com>
2022-07-04 15:05:27 -0300

    Melhorando iframes, tempo de espera e calculando

--- src/main/java/br/com/skw/folha/tests/CalcIndMensalNormalTest.java
+++ src/main/java/br/com/skw/folha/tests/CalcIndMensalNormalTest.java
@@ -1,5 +1,6 @@
 package br.com.skw.folha.tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,7 +22,7 @@ public class CalcIndMensalNormalTest extends BaseTest {
 		getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		menuPage.abreTelaCalculoFolha();
 		//getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		Thread.sleep(2000);
+		Thread.sleep(6000);
 		calcPage.abreCalculoIndividual();
 		getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		calcPage.abreCardCalculoIndividualMensal();
@@ -29,11 +30,17 @@ public class CalcIndMensalNormalTest extends BaseTest {
 		calcPage.preencheReferencia("07/2022");
 		calcPage.preencheDataPagamento("29/07/2022");
 		
-		calcPage.preencheCodEmp("2");
-		//calcPage.preencheCodFunc("14");
+		calcPage.preencheCodEmp("2");		
+		Thread.sleep(5000);
+		//System.out.println(calcPage.pegaNomeEmp());
+		//Assertions.assertEquals("EMPRESA HOMOLOGACAO 4.12", calcPage.pegaNomeEmp());
+		calcPage.preencheCodFunc("14");
 		Thread.sleep(5000);
 		
-		
+		calcPage.clicarBtnProximo();
+		Thread.sleep(2000);
+		calcPage.clicarBtnCalcular();
+		Thread.sleep(5000);		
 	}
 	
 }
