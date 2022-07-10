a3de2913ccaa3e5ef23e3786f6828546742562ae
Filipe Campos <felipetwo@gmail.com>
2022-07-09 22:05:56 -0300

    Ajustando os assertions da sessão proventos

--- src/main/java/br/com/skw/folha/tests/CalcIndMensalNormalTest.java
+++ src/main/java/br/com/skw/folha/tests/CalcIndMensalNormalTest.java
@@ -3,6 +3,7 @@ package br.com.skw.folha.tests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import br.com.skw.folha.core.BaseTest;
 import br.com.skw.folha.pages.CalculoPage;
@@ -31,16 +32,29 @@ public class CalcIndMensalNormalTest extends BaseTest {
 		calcPage.preencheDataPagamento("29/07/2022");
 		
 		calcPage.preencheCodEmp("2");		
-		Thread.sleep(5000);
+		Thread.sleep(3000);
 		//System.out.println(calcPage.pegaNomeEmp());
 		//Assertions.assertEquals("EMPRESA HOMOLOGACAO 4.12", calcPage.pegaNomeEmp());
 		calcPage.preencheCodFunc("14");
-		Thread.sleep(5000);
+		Thread.sleep(3000);
 		
 		calcPage.clicarBtnProximo();
 		Thread.sleep(2000);
 		calcPage.clicarBtnCalcular();
 		Thread.sleep(5000);		
+		
+		calcPage.clicarSessaoProventos();
+		Thread.sleep(1000);
+		Assertions.assertEquals("1", calcPage.pegarValoresDeCalculo("(//div[@id='idProv']//p[@class='center-self ng-binding'])[1]"));
+		Assertions.assertEquals("Salario Base", calcPage.pegarValoresDeCalculo("(//div[@id='idProv']//p[@class='center-self ng-binding'])[2]"));
+		Assertions.assertEquals("R$ 5.000,00", calcPage.pegarValoresDeCalculo("//p[@id='prov-val-10']"));
+		Assertions.assertEquals("30.000", calcPage.pegarValoresDeCalculo("//p[@id='prov-ind-1']"));
+		Assertions.assertEquals("11", calcPage.pegarValoresDeCalculo("(//div[@id='idProv']//p[@class='center-self ng-binding'])[3]"));
+		Assertions.assertEquals("Periculosidade", calcPage.pegarValoresDeCalculo("(//div[@id='idProv']//p[@class='center-self ng-binding'])[4]"));
+		Assertions.assertEquals("R$ 1.500,00", calcPage.pegarValoresDeCalculo("//p[@id='prov-val-110']"));
+		Assertions.assertEquals("30.000", calcPage.pegarValoresDeCalculo("//p[@id='prov-ind-11']"));
+		Thread.sleep(1000);
+		calcPage.clicarSessaoProventos();
 	}
 	
 }
